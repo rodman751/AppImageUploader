4763dd3b5fa20c65466323125b82d5962645925b
Rodmansito <78875773+rodman751@users.noreply.github.com>
2024-07-27 20:40:39 -0500

    actualizacion css

--- Entidades/Image.cs
+++ Entidades/Image.cs
@@ -1,10 +1,14 @@
-﻿namespace Entidades
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entidades
 {
     public class Image
     {
         // Properties
         public int Id { get; set; }
+        [Display(Name = "Nombre de la Imagen")]
         public string FileName { get; set; }
+        [Display(Name = "Imagen")]
         public byte[] Data { get; set; }
 
         
